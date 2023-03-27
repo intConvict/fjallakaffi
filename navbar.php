98713ff74c4287918daec5a8a5f4c5731de87bb7
intConvict <joostvangent911@gmail.com>
2023-03-27 13:30:32 +0200

    init commit

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="links/css/navbar.css">
+</head>
+<body>
+  <header class='navbar_wrapper'>
+    <div class='navbar_wrapper-titleholder'>
+      <a>
+        <h1>fjallakaffi</h1>
+        <h3>recipes</h3>
+      </a>
+    </div>
+    <div class='navbar_wrapper-accountholder'>
+      <a>
+        <h3>
+          sign in
+        </h3>
+      </a>
+      <a href="signup.php">
+        <h3>
+          sign up
+        </h3>
+      </a>
+    </div>
+  </header>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="links/css/index.css">
+  <link rel="stylesheet" href="links/css/signup.css">
+</head>
+<body>
+  <div class="signup-wrapper">
+    <div class='titleholder'>
+      <a href="index.php">
+        <h1>fjallakaffi</h1>
+        <h3>recipes</h3>
+      </a>
+    </div>
+    <div class='signupholder'>
+      <div>
+        <h1>
+          Sign up :)
+        </h1>
+      </div>
+      <div>
+      </div>
+    </div>
+  </div>
+  <div class="signin-wrapper">
+  </div>
+</body>
+</html>
+
+<?php
+?>
